9125cafc488ef56da59e68d52824b9ecb1a6af44
ChungViet <misterrealdragonroarr@gmail.com>
2020-09-05 23:33:49 +0300

    homework2

---
+++ Homework/homework2.py
@@ -0,0 +1,10 @@
+a= """
+\\a     Bell (alert)
+\\b     backspace
+\\n     New Line
+\\t     Horizontal tab
+\\\     Backslash \ 
+\\'     single quotation mark
+\\"     double quotation mark
+"""
+print(a)
